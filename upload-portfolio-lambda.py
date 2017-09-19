8c888315e008995a113eacf2ea5005f18c75002c
Robert Baker <rsbaker256@gmail.com>
2017-09-19 01:36:40 -0600

    added babel.html

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -19,7 +19,8 @@ def lambda_handler(event, context):
 
         if job:
             for artifact in job["data"]["inputArtifacts"]:
-                if artifact["name"] == "MyBuildApp":
+                print "artifact name: " + artifact["name"]
+                if artifact["name"] == "MyAppBuild":
                     location = artifact["location"]["s3Location"]
 
         print "Building portfolio from " + str(location)
